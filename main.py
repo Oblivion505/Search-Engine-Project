2a153232ce147cdcca053737dd0fc8482aa70acf
Josh <47038778+Oblivion505@users.noreply.github.com>
2026-01-29 02:37:14 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+import sys
+
 from gui import Gui
 from inverted_index import Inverted_index
-from tf_idf import Tf_idf
+from rankings.tf_idf import Tf_idf
 
 import utils
 
+# Command Line Arguments
+args: list = sys.argv
+
+# { filename : text } pairs
 docs: dict = utils.get_docs("test_data")
 
 index: Inverted_index = Inverted_index()
 
-# Creates new index structure from documents if saved index not found
-if not index.load():
+# New index created if saved index doesn't exist or user enters 'new' as an argument
+if not index.load() or (len(args) > 1 and args[1] == "new"):
 
     index.create(docs)
     index.save()
 
-ranking: Tf_idf = Tf_idf(index)
+    print("New Index Saved")
 
-gui: Gui = Gui(ranking)
-gui.run()
+else:
+
+    ranking: Tf_idf = Tf_idf(index)
+
+    gui: Gui = Gui(ranking)
+    gui.run()
